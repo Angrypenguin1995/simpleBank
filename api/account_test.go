9282ae757aba808d00165007fd7e2268a2a9e9e3
angrypenguin1995 <satish.chandragg@gmail.com>
2023-05-31 19:27:34 +0530

    completed implementing Paseto ad Uer login API

--- api/account_test.go
+++ api/account_test.go
@@ -87,7 +87,7 @@ func TestGetAccountAPI(t *testing.T) {
 			tc.buildStubs(store)
 
 			// start test server and send request
-			server := NewServer(store)
+			server := newTestServer(t, store)
 			recorder := httptest.NewRecorder()
 
 			url := fmt.Sprintf("/accounts/%d", tc.accountID)
@@ -193,7 +193,7 @@ func TestCreateAccountAPI(t *testing.T) {
 			store := mockdb.NewMockStore(control)
 			tc.buildStubs(store)
 
-			server := NewServer(store)
+			server := newTestServer(t, store)
 			recorder := httptest.NewRecorder()
 
 			//since this is a post call, there will be data involved, so lets unmarshal the data first
@@ -304,7 +304,7 @@ func TestListAccountsAPI(t *testing.T) {
 			store := mockdb.NewMockStore(control)
 			tc.buildStubs(store)
 
-			server := NewServer(store)
+			server := newTestServer(t, store)
 			recorder := httptest.NewRecorder()
 
 			url := fmt.Sprintf("/accounts")
--- api/server.go
+++ api/server.go
@@ -1,26 +1,50 @@
 package api
 
 import (
+	"fmt"
+
 	db "github.com/angrypenguin1995/simple__bank/db/sqlc"
+	"github.com/angrypenguin1995/simple__bank/token"
+	"github.com/angrypenguin1995/simple__bank/util"
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/validator/v10"
 )
 
 type Server struct {
-	store  db.Store
-	router *gin.Engine
+	store      db.Store
+	tokenMaker token.Maker
+	router     *gin.Engine
+	config     util.Config
 }
 
-func NewServer(store db.Store) *Server {
-	server := &Server{store: store}
-	router := gin.Default()
+func NewServer(config util.Config, store db.Store) (*Server, error) {
+	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
+	// tokenMaker, err := token.NewJWTMaker(config.TokenSymmetricKey) //uncomment this line and comment above line for using JWT isntead of PASETO for token
+	if err != nil {
+		return nil, fmt.Errorf("Failed to create a tokenmaker: %w", err)
+	}
+
+	server := &Server{
+		config:     config,
+		store:      store,
+		tokenMaker: tokenMaker,
+	}
 
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
 		v.RegisterValidation("currency", validCurrency)
 	}
 
+	server.setupRouter()
+	return server, nil
+}
+
+func (server *Server) setupRouter() {
+
+	router := gin.Default()
+
 	router.POST("/users", server.createUser)
+	router.POST("/users/login", server.loginUser)
 
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
@@ -29,7 +53,6 @@ func NewServer(store db.Store) *Server {
 	router.POST("/transfers", server.createTransfer)
 
 	server.router = router
-	return server
 }
 
 // Start function runs the HTTP Server on specific address as server is a private variable scoped to this package
